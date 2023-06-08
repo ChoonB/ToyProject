65791bd59fa8f4dc5b0aa3de479d6a2fb91c1b5e
ChoonB <shgkdc@gmail.com>
2023-06-08 12:10:04 +0900

    Feat : 게시글 CRUD 전부 구현 update와 delete post 추가 구현

--- src/main/java/com/example/choonb/domain/post/controller/PostController.java
+++ src/main/java/com/example/choonb/domain/post/controller/PostController.java
@@ -4,12 +4,15 @@ import com.example.choonb.domain.post.dto.EntirePostResponseDto;
 import com.example.choonb.domain.post.dto.PostRequestDto;
 import com.example.choonb.domain.post.dto.PostResponseDto;
 import com.example.choonb.domain.post.service.PostService;
+import com.example.choonb.domain.user.dto.MessageResponseDto;
 import com.example.choonb.global.security.UserDetailsImpl;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,20 +26,37 @@ public class PostController {
 
   private final PostService postService;
 
+//  글쓰기
   @PostMapping("")
   public PostResponseDto writePost(@RequestBody @Valid PostRequestDto postRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails){
     return postService.writePost(postRequestDto, userDetails.getUser());
   }
 
+//  상세조회
   @GetMapping("/{id}")
   public PostResponseDto getPost(@PathVariable Long id) {
     return postService.getPost(id);
   }
 
+//  전체조회
   @GetMapping("")
   public List<EntirePostResponseDto> getPostList(){
     return postService.getPostList();
+  }
+
+//  글 수정
+  @PatchMapping("/{id}")
+  public PostResponseDto updatePost(@RequestBody @Valid PostRequestDto postRequestDto,
+      @PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return postService.updatePost(id, postRequestDto, userDetails.getUser());
+  }
+
+//  글 삭제
+  @DeleteMapping("/{id}")
+  public MessageResponseDto deletePost(@PathVariable Long id,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return postService.deletePost(id, userDetails.getUser());
   }
 
 }
--- src/main/java/com/example/choonb/domain/post/service/PostService.java
+++ src/main/java/com/example/choonb/domain/post/service/PostService.java
@@ -5,11 +5,14 @@ import com.example.choonb.domain.post.dto.PostRequestDto;
 import com.example.choonb.domain.post.dto.PostResponseDto;
 import com.example.choonb.domain.post.entity.Post;
 import com.example.choonb.domain.post.repository.PostRepository;
+import com.example.choonb.domain.user.dto.MessageResponseDto;
 import com.example.choonb.domain.user.entity.User;
+import com.example.choonb.domain.user.entity.UserRoleEnum;
 import com.example.choonb.global.exception.CustomException;
 import com.example.choonb.global.exception.ExceptionType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -35,4 +38,34 @@ public class PostService {
         .map(EntirePostResponseDto::new)
         .toList();
   }
+
+  public PostResponseDto updatePost(Long id, PostRequestDto postRequestDto, User user) {
+    Post post = findPostById(id);
+    checkUser(post, user);
+
+    post.update(postRequestDto);
+    postRepository.saveAndFlush(post);
+    return new PostResponseDto(post);
+
+  }
+
+  public MessageResponseDto deletePost(Long id, User user) {
+    Post post = findPostById(id);
+    checkUser(post, user);
+    postRepository.delete(post);
+    return new MessageResponseDto(HttpStatus.NO_CONTENT, "게시글이 정상적으로 삭제되었습니다.");
+  }
+
+
+  private Post findPostById(Long id){
+    return postRepository.findById(id).orElseThrow(
+        () -> new CustomException(ExceptionType.NOT_FOUND_POST_EXCEPTION)
+    );
+  }
+  private void checkUser(Post post, User user) {
+    if (!user.getRole().equals(UserRoleEnum.ADMIN) && !user.getId().equals(post.getUser().getId())) {
+      throw new CustomException(ExceptionType.USER_UNAUTHORIZED_EXCEPTION);
+    }
+  }
+
 }
